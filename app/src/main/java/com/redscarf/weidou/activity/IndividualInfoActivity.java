227d78f0460a906311103c7fc3ca2a20ce36880a
yiva <wyhtsh@gmail.com>
2016-04-03 01:53:11 +0800

    set layout view

--- app/src/main/java/com/redscarf/weidou/activity/IndividualInfoActivity.java
+++ app/src/main/java/com/redscarf/weidou/activity/IndividualInfoActivity.java
@@ -3,6 +3,7 @@ package com.redscarf.weidou.activity;
 import android.os.Bundle;
 
 /**
+ * 个人信息界面
  * Created by yeahwang on 2016/3/25.
  */
 public class IndividualInfoActivity extends BaseActivity{
@@ -11,6 +12,7 @@ public class IndividualInfoActivity extends BaseActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual_info);
     }
 
     @Override
